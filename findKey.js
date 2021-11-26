99d4e66841d446c11a82272703bf9ca3efbd2458
Marquis Welch-Fraser <marquis.240p@gmail.com>
2021-11-26 10:08:46 -0500

    finds key from object and returns true or undefind

---
+++ findKey.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟩🟩🟩 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = (object , callback) => {
+  for (const key in object) {
+        console.log(object[key])
+    if (callback(object[key])) {
+      return key;
+    } 
+  }
+}
+
+const result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 10) // => undefind
+
+assertEqual(result, "noma");
+assertEqual(result1, undefined);
+
