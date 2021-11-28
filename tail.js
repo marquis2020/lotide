1bc80182d7de635adfd0cd1f3ce736120ada93ca
Marquis Welch-Fraser <marquis.240p@gmail.com>
2021-11-28 02:27:07 -0500

    fixed

--- tail.js
+++ tail.js
@@ -1,19 +1,42 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('🟩🟩🟩 Assertion Passed: [actual] === [expected]');
-  } else if (actual != expected) {
-    console.log('🔴🔴🔴 Assertion Failed: [actual] !== [expected]');
-  } 
+    console.log(`🟩🟩🟩 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
 };
 
-const tail = function(one,two){
-  const tailOfOne = one[one.lenght -1]
-  return tailOfOne;
-}
+ const tail = function(one,two){
 
-assertEqual(tail([5,6,7]), 5);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(tail([2]), 2)
-assertEqual(tail([]), );
-assertEqual(tail([5,6,7]), 6);
+  if (one.length === 0) {
+    return undefined;
+  }
+  if (one.length === 1) {
+    spOne = one[0];
+    return spOne;
+  }
+   const tailOfOne = one.slice(-1)
+   return tailOfOne;
+ }
 
+ assertEqual(tail([5,6,7]), 5);
+ assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
+ assertEqual(tail([2]), 2)
+ assertEqual(tail([]), );
+ assertEqual(tail([5,6,7]), 6);
+
+// const tail = function(array) {
+//   array = array.slice(1);
+//   return array;
+// };
+// const assertEqual = function(a, b) {
+//   if (a === b) {
+//     console.log('🟩 Passed');
+//   } else {
+//     console.log('🟥 Failed');
+//   }
+// };
+// assertEqual(tail([5, 6, 7]), [6, 7]);
+// assertEqual(tail([5, 6, 7]), [5, 6, 7]);
+// assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+// assertEqual(tail([5, 6, 7]), 6);
