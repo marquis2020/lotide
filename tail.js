d33a57cfd7cc29d0368c4dbaf8342cd4344aac7c
Marquis Welch-Fraser <marquis.240p@gmail.com>
2021-11-28 02:28:44 -0500

    fixed

--- tail.js
+++ tail.js
@@ -25,18 +25,3 @@ const assertEqual = function(actual, expected) {
  assertEqual(tail([]), );
  assertEqual(tail([5,6,7]), 6);
 
-// const tail = function(array) {
-//   array = array.slice(1);
-//   return array;
-// };
-// const assertEqual = function(a, b) {
-//   if (a === b) {
-//     console.log('🟩 Passed');
-//   } else {
-//     console.log('🟥 Failed');
-//   }
-// };
-// assertEqual(tail([5, 6, 7]), [6, 7]);
-// assertEqual(tail([5, 6, 7]), [5, 6, 7]);
-// assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
-// assertEqual(tail([5, 6, 7]), 6);
