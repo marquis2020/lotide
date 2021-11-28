9013bc8ec013f826d8813e42d38c253f2027a504
Marquis Welch-Fraser <marquis.240p@gmail.com>
2021-11-28 13:02:55 -0500

    assertObjectsEqual

---
+++ assertObjectsEqual.js
@@ -0,0 +1,33 @@
+
+const eqObjects = function(object1, object2) {
+  // returning an array of objects and compares their length
+  if (Object.keys(object1).length !== Object.keys(object2).length) {
+    return false;
+  }
+  for (const key of Object.keys(object1)) {
+    //checking if the value at a certen key is an array
+    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+      if (!eqArrays(object1[key], object2[key])) {
+        return false;
+      }
+      //if the values at these keys are not equal return false
+    } else if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// FUNCTION IMPLEMENTATION
+const assertObjectsEqual = function(object1, object2) {
+  // Implement me!
+  const inspect = require('util').inspect;
+  if (eqObjects(object1 , object2))
+    return console.log(`✅✅✅ Assertion Passed: ${inspect(object1)} === ${inspect(object2)}`);
+  else
+    return console.log(`🛑🛑🛑 Assertion Failed: ${inspect(object1)} !== ${inspect(object1)}`);
+};
+//assertObjectsEqual();
+ const cd = { c: "1", d: "2" };
+ const dc = { d: "2", c: "1" };
+ assertObjectsEqual(cd , dc );
