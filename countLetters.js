d1ce0157428aa21fee902b7fdc7e813ed1923a0a
Marquis Welch-Fraser <marquis.240p@gmail.com>
2021-11-28 03:47:46 -0500

    counts how many times a letter is repeated

---
+++ countLetters.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟩🟩🟩 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(stringToCount) {
+  const results = {}
+
+  for(const charaters of stringToCount){
+    if (charaters === " ") {}
+      if (results[charaters]) {
+        results[charaters] += 1;
+      } else {
+        results[charaters] = 1;
+      }
+    
+  }
+  console.log(results)
+  return results;
+};
+
+const stringToCheck = "lighthouse in the house";
+const result1 = countLetters(stringToCheck);
+
+assertEqual(result1["t"], 1); // should fail
+assertEqual(result1["h"], 4); // should pass
+assertEqual(result1["e"], 3); // should pass
+assertEqual(result1["l"], 2); // should fail
