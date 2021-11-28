0334b6103eae66ed3eb194bb7948f4e529e69e3b
Marquis Welch-Fraser <marquis.240p@gmail.com>
2021-11-28 05:29:41 -0500

    finding the key by its value pair

---
+++ findKeyByValue.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟩🟩🟩 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKeyByValue =function(object , value){
+  // using 
+  return Object.keys(object).find(function (key) {
+      return object[key] === value;
+    });
+}
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
