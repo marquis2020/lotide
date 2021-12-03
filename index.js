b4d79fb310b83776e2d9fe812fc8024e58cdd423
Marquis Welch-Fraser <marquis.240p@gmail.com>
2021-12-03 10:00:54 -0500

    ES6 notation

--- index.js
+++ index.js
@@ -17,22 +17,50 @@ const findKey = require('./findKey')
 const findKeyByValue = require('./findKeyByValue')
 
 
-module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  assertEqual: assertEqual,
-  assertArraysEqual: assertArraysEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  letterPositions: letterPositions,
-  map: map,
-  takeUntil: takeUntil,
-  without: without,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
 
+// e6 shorthand property values
+module.exports = {
+  head,
+  tail,
+  middle,
+  eqArrays,
+  assertEqual,
+  assertArraysEqual,
+  assertObjectsEqual,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  countLetters,
+  countOnly,
+  letterPositions,
+  map,
+  takeUntil,
+  without
 };
+
+
+
+
+
+
+
+
+// module.exports = {
+//   head: head,
+//   tail: tail,
+//   middle: middle,
+//   assertEqual: assertEqual,
+//   assertArraysEqual: assertArraysEqual,
+//   assertObjectsEqual: assertObjectsEqual,
+//   eqArrays: eqArrays,
+//   eqObjects: eqObjects,
+//   countLetters: countLetters,
+//   countOnly: countOnly,
+//   letterPositions: letterPositions,
+//   map: map,
+//   takeUntil: takeUntil,
+//   without: without,
+//   findKey: findKey,
+//   findKeyByValue: findKeyByValue,
+
+// };
